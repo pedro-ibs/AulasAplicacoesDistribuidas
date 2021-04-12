f3a8e28c8140f52088eff964184581581f873e77
pedro_ibs <pedro.igor.ifsp@gmail.com>
2021-04-11 22:36:28 -0300

    add atividades 1 e 2

---
+++ aula03_atv1/src/main/java/br/edu/ifsp/aula03_atv1/Aula03Atv1Application.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.aula03_atv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula03Atv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula03Atv1Application.class, args);
+	}
+
+}
---
+++ aula03_atv1/src/test/java/br/edu/ifsp/aula03_atv1/Aula03Atv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.aula03_atv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula03Atv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
